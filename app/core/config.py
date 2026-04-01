de0cf0d483a956feb3d91c8d928e91cc377a205c
shivesh93212 <0105cd231185@oriental.ac.in>
2026-04-01 23:23:56 +0530

    initial

---
+++ app/core/config.py
@@ -0,0 +1,20 @@
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    APP_NAME=os.getenv("APP_NAME")
+    DEBUG: bool =os.getenv("DEBUG") =="True"
+
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+
+    CLERK_JWKS_URL:str = os.getenc("CLERK_JWKS_URL")
+
+    CLOUDINARY_CLOUD_NAME: str = os.getenv("CLOUDINARY_CLOUD_NAME")
+    CLOUDINARY_API_KEY: str = os.getenv("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET: str = os.getenv("CLOUDINARY_API_SECRET")
+
+    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
+
+    Settings=Settings()
